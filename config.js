c170811fa2308e61900034813d2422b1573c353e
takoizume <92854115+takoizume@users.noreply.github.com>
2021-10-20 16:23:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "20/10",
+    introTitle: 'Các bạn nữ của gia đình HWC',
+    introDesc: `abc
+    abc
+   abc
+abc`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'HWC chúc bạn hãy có một ngày 20/10 thật vui vẻ và hạnh phúc bên người thân nhá 🥰',
+    desc: 'omke khum? ',
+    btnYes: 'Nhất định <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu HWC <3',
+    btnReply: '(minh bi ep phai gui)',
+    reply: 'Mình rất iu HWC <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 8h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://HWC gi do
 }
