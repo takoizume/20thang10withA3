a40678f13e50002ac2650ee69590ae8367421b49
takoizume <92854115+takoizume@users.noreply.github.com>
2021-10-20 23:37:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const CONFIG = {
     titleWeb: "Mừng ngày Phụ nữ Việt Nam",
     introTitle: 'thân gửi các bạn nữ A3',
     introDesc: `dù ngày hôm nay chỉ còn một xíu...
-    một mẩu có khi chừng ba mươi phút thôi...
-    nhưng nếu đó là một mẩu tốt lành
-    thì hẳn ngày hôm nay vẫn là một ngày tốt đẹp`,
-    btnIntro: '^^HiHi^^',
+    một mẩu có khi chừng ba mươi phút thôi,
+    nhưng nếu đó là một mẩu tốt lành thì mình tin
+    hẳn ngày hôm nay vẫn là một ngày tốt đẹp nhỉ`,
+    btnIntro: '^^chac z^^',
     title: 'hãy chắc chắn rằng hôm nay là một ngày vui vẻ nhé?',
     desc: 'omke khum? trả lời.',
     btnYes: 'Nhất định <33',
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'tuy khum còn gặp nhau nữa nhưng liệu có bao giờ các cậu nhớ về nhau?',
     btnReply: 'send anw',
     reply: '... chắc là có đó <3',
-    mess: 'yess mình biết chắc đó là lời từ đáy lòng chứ không hề ép buộc mà 🥰',
+    mess: 'yess mình biết chắc đó là lời từ tự nguyện từ đáy lòng mà 🥰',
     messDesc: 'truyền thuyết kể rằng nếu bạn cố nhớ thứ gì thì nó sẽ hiện ra ngay trước mắt ',
-    btnAccept: '?',
+    btnAccept: 'thật sự là khum nhớ rì hết',
     messLink: 'https://www.facebook.com/groups/165563590670218/posts/343033039589938/' //link mess của các bạn. VD: https://HWC gi do
 }
